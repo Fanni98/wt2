d3727dc9b7c086dc33087aff31370e4e0ad0cd99
Fanni98 <gozfanni@gmail.com>
2020-08-31 16:04:39 +0200

    cccccccccc

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,6 +31,7 @@ router.post("/register", (req, res) => {
     if (!isValid) {
       return res.status(400).json([errors]);
     }
+ 
   User.findOne({ name: req.body.name }).then(user => {
       if (user) {
         return res.status(400).json({ name: "name already exists" });
